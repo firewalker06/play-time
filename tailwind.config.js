b9e21e9694716d5f79a977027758aa1300516d63
Didik Wicaksono <didik.wicaksono@gmail.com>
2020-10-16 05:29:39 +0700

    Hide play card container on print

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      screens: {
+        'print': {'raw': 'print'},
+      }
+    }
+  },
+  variants: {},
+  plugins: [],
+}
